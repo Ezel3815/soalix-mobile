0bf2f92f996477f5021dc9c3bfe1ae72dccb7682
Ezel3815 <skukzleenii@gmail.com>
2026-09-06 14:28:39 +0300

    Update app_drawer.dart

--- lib/screens/app_drawer.dart
+++ lib/screens/app_drawer.dart
@@ -1,8 +1,8 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:upgrade/controllers/api_controller.dart';
 import 'package:upgrade/controllers/main_controller.dart';
@@ -19,36 +19,49 @@ class AppDrawer extends StatelessWidget {
     final width = MediaQuery.sizeOf(context).width;
     final mainController = Get.find<MainController>();
 
+    TextStyle itemStyle({Color? color}) => TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w600,
+          color: color ?? AppColor.textPrimary,
+        );
+
+    Widget iconBadge(IconData icon, {Color badgeColor = Colors.black}) {
+      return Container(
+        width: 40,
+        height: 40,
+        decoration: BoxDecoration(
+          color: badgeColor,
+          shape: BoxShape.circle,
+        ),
+        child: Icon(icon, color: Colors.white, size: 20),
+      );
+    }
+
     return Container(
-      width: width * .7,
-      color:  AppColor.scaffoldBackgroundColor,
+      width: width * .78,
+      color: AppColor.scaffoldBackgroundColor,
       child: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 10),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              const SizedBox(height: 8),
               Image.asset(
                 "lib/assests/images/drawer_logo.png",
                 width: 150,
                 height: 100,
                 fit: BoxFit.contain,
               ),
+              const SizedBox(height: 8),
               ListTile(
-                leading: SvgPicture.asset(
-                  "lib/assests/svgs/home.svg",
-                  colorFilter:
-                      const ColorFilter.mode(Colors.black, BlendMode.srcIn),
-                  width: 30,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                title: const Text(
-                  "Explore",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
+                leading: iconBadge(
+                  PhosphorIcons.house(PhosphorIconsStyle.bold),
                 ),
+                title: Text("Explore", style: itemStyle()),
                 onTap: () {
                   Get.back();
                   Get.until(
@@ -60,24 +73,17 @@ class AppDrawer extends StatelessWidget {
                 },
               ),
               ListTile(
-                leading: SvgPicture.asset(
-                  "lib/assests/svgs/document.svg",
-                  colorFilter:
-                      const ColorFilter.mode(Colors.black, BlendMode.srcIn),
-                  width: 30,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                title: const Text(
-                  "Document",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
+                leading: iconBadge(
+                  PhosphorIcons.fileText(PhosphorIconsStyle.bold),
                 ),
+                title: Text("Document", style: itemStyle()),
                 onTap: () {
                   Get.back();
                   Get.until(
-                        (route) {
+                    (route) {
                       return Get.currentRoute == AppRoutes.mainRoute;
                     },
                   );
@@ -85,24 +91,17 @@ class AppDrawer extends StatelessWidget {
                 },
               ),
               ListTile(
-                leading: SvgPicture.asset(
-                  "lib/assests/svgs/cloud.svg",
-                  colorFilter:
-                      const ColorFilter.mode(Colors.black, BlendMode.srcIn),
-                  width: 30,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                title: const Text(
-                  "Create",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
+                leading: iconBadge(
+                  PhosphorIcons.cloudArrowUp(PhosphorIconsStyle.bold),
                 ),
+                title: Text("Create", style: itemStyle()),
                 onTap: () {
                   Get.back();
                   Get.until(
-                        (route) {
+                    (route) {
                       return Get.currentRoute == AppRoutes.mainRoute;
                     },
                   );
@@ -110,136 +109,61 @@ class AppDrawer extends StatelessWidget {
                 },
               ),
               ListTile(
-                leading: SvgPicture.asset(
-                  "lib/assests/svgs/camera.svg",
-                  colorFilter:
-                      const ColorFilter.mode(Colors.black, BlendMode.srcIn),
-                  width: 30,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                title: const Text(
-                  "Enter Code",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
+                leading: iconBadge(
+                  PhosphorIcons.camera(PhosphorIconsStyle.bold),
                 ),
+                title: Text("Enter Code", style: itemStyle()),
                 onTap: () {
                   Get.back();
                   Get.dialog(const InterCodeDialog());
                 },
               ),
-              // ListTile(
-              //   leading: SvgPicture.asset(
-              //     "lib/assests/svgs/folder.svg",
-              //     colorFilter:
-              //         const ColorFilter.mode(Colors.black, BlendMode.srcIn),
-              //     width: 30,
-              //   ),
-              //   title: const Text(
-              //     "Buy Code",
-              //     style: TextStyle(
-              //       fontSize: 18,
-              //       fontWeight: FontWeight.bold,
-              //       color: Colors.black,
-              //     ),
-              //   ),
-              // ),
-              const Divider(),
+              Divider(
+                color: Colors.black.withOpacity(0.08),
+                thickness: 1,
+                height: 24,
+              ),
               ListTile(
-                leading: SvgPicture.asset(
-                  "lib/assests/svgs/like.svg",
-                  colorFilter:
-                      const ColorFilter.mode(Colors.black, BlendMode.srcIn),
-                  width: 30,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                title: const Text(
-                  "Share This App",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
+                leading: iconBadge(
+                  PhosphorIcons.thumbsUp(PhosphorIconsStyle.bold),
                 ),
+                title: Text("Share This App", style: itemStyle()),
                 onTap: () {
                   Share.share("Checkout this app in https://googleplay.com");
                 },
               ),
               ListTile(
-                onTap: () async{
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                onTap: () async {
                   if (!await launchUrl(Uri.parse("https://t.me/Ghalia510"))) {
-
-                  log('Could not launch https://t.me/Ghalia510');
+                    log('Could not launch https://t.me/Ghalia510');
                   }
                 },
-                leading: SvgPicture.asset(
-                  "lib/assests/svgs/help.svg",
-                  colorFilter:
-                      const ColorFilter.mode(Colors.black, BlendMode.srcIn),
-                  width: 30,
+                leading: iconBadge(
+                  PhosphorIcons.question(PhosphorIconsStyle.bold),
                 ),
-                title: const Text(
-                  "Help & Feedback",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
+                title: Text("Help & Feedback", style: itemStyle()),
               ),
-              // ListTile(
-              //   leading: Transform.rotate(
-              //     angle: 6,
-              //     child: SvgPicture.asset(
-              //       "lib/assests/svgs/announcement.svg",
-              //       colorFilter:
-              //           const ColorFilter.mode(Colors.black, BlendMode.srcIn),
-              //       width: 30,
-              //     ),
-              //   ),
-              //   title: const Text(
-              //     "Announcement",
-              //     style: TextStyle(
-              //       fontSize: 18,
-              //       fontWeight: FontWeight.bold,
-              //       color: Colors.black,
-              //     ),
-              //   ),
-              // ),
-              // ListTile(
-              //   leading: SvgPicture.asset(
-              //     "lib/assests/svgs/gift.svg",
-              //     colorFilter:
-              //         const ColorFilter.mode(Colors.black, BlendMode.srcIn),
-              //     width: 30,
-              //   ),
-              //   title: const Text(
-              //     "Claim Your Reward",
-              //     style: TextStyle(
-              //       fontSize: 18,
-              //       fontWeight: FontWeight.bold,
-              //       color: Colors.black,
-              //     ),
-              //   ),
-              // ),
               ListTile(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
                 onTap: () {
                   ApiController.logout();
                 },
-                leading: SvgPicture.asset(
-                  "lib/assests/svgs/logout.svg",
-                  colorFilter:
-                      const ColorFilter.mode(Colors.red, BlendMode.srcIn),
-                  width: 30,
+                leading: iconBadge(
+                  PhosphorIcons.signOut(PhosphorIconsStyle.bold),
+                  badgeColor: Colors.red,
                 ),
-                title: const Text(
-                  "Log Out",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.red,
-                  ),
-                ),
+                title: Text("Log Out", style: itemStyle(color: Colors.red)),
               ),
             ],
           ),

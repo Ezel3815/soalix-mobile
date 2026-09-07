889ddeaf38f2de178823f67be60cfc010615273d
Ezel3815 <skukzleenii@gmail.com>
2026-09-07 13:13:43 +0300

    Replace DocumentScreen with ProfileScreen in MainController

--- lib/controllers/main_controller.dart
+++ lib/controllers/main_controller.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:upgrade/screens/creat_deck/create_deck_screen.dart';
-import 'package:upgrade/screens/document_screen.dart';
+import 'package:upgrade/screens/profile_screen.dart';
 import 'package:upgrade/screens/years_screen.dart';
 
 class MainController extends GetxController {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   final RxInt _page = 0.obs;
-
   int get page => _page.value;
-
   set page(value) => _page.value = value;
 
   onChangePage(index) async {
-
     if (index == 3) {
       scaffoldKey.currentState?.openDrawer();
       return;
@@ -24,6 +21,6 @@ class MainController extends GetxController {
   final pages = const [
     YearsScreen(),
     CreateDeckScreen(),
-    DocumentScreen(),
+    ProfileScreen(),
   ];
 }

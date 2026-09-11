cddb861d26f585e9cfe5d8e474860e4ba3f07065
Ezel3815 <skukzleenii@gmail.com>
2026-09-11 17:48:38 +0300

    Refactor app drawer styles and navigation logic

--- lib/screens/app_drawer.dart
+++ lib/screens/app_drawer.dart
@@ -1,3 +1,4 @@
+
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
@@ -20,21 +21,31 @@ class AppDrawer extends StatelessWidget {
     final mainController = Get.find<MainController>();
 
     TextStyle itemStyle({Color? color}) => TextStyle(
-          fontSize: 16,
+          fontSize: 15,
           fontWeight: FontWeight.w600,
           color: color ?? AppColor.textPrimary,
         );
 
-    Widget iconBadge(IconData icon, {Color badgeColor = Colors.black}) {
+    Widget iconBadge(IconData icon, {Color? badgeColor}) {
       return Container(
-        width: 40,
-        height: 40,
+        width: 38,
+        height: 38,
         decoration: BoxDecoration(
-          color: badgeColor,
+          color: (badgeColor ?? AppColor.greenColor).withOpacity(0.12),
           shape: BoxShape.circle,
         ),
-        child: Icon(icon, color: Colors.white, size: 20),
+        child: Icon(
+          icon,
+          color: badgeColor ?? AppColor.darkGreenColor,
+          size: 19,
+        ),
       );
+    }
+
+    void goToTab(int index) {
+      Get.back();
+      Get.until((route) => Get.currentRoute == AppRoutes.mainRoute);
+      mainController.onChangePage(index);
     }
 
     return Container(
@@ -46,75 +57,70 @@ class AppDrawer extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(height: 8),
-              Image.asset(
-                "lib/assests/images/drawer_logo.png",
-                width: 150,
-                height: 100,
-                fit: BoxFit.contain,
+              const SizedBox(height: 12),
+              Padding(
+                padding: const EdgeInsets.only(left: 6),
+                child: Image.asset(
+                  "lib/assests/images/drawer_logo.png",
+                  width: 150,
+                  height: 102,
+                  fit: BoxFit.contain,
+                  alignment: Alignment.centerLeft,
+                ),
               ),
-              const SizedBox(height: 8),
+              const SizedBox(height: 16),
               ListTile(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12),
                 ),
-                leading: iconBadge(
-                  PhosphorIcons.house(PhosphorIconsStyle.bold),
-                ),
-                title: Text("Explore", style: itemStyle()),
-                onTap: () {
-                  Get.back();
-                  Get.until(
-                    (route) {
-                      return Get.currentRoute == AppRoutes.mainRoute;
-                    },
-                  );
-                  mainController.onChangePage(0);
-                },
+                leading: iconBadge(PhosphorIcons.house(PhosphorIconsStyle.bold)),
+                title: Text("Home", style: itemStyle()),
+                onTap: () => goToTab(0),
               ),
               ListTile(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12),
                 ),
-                leading: iconBadge(
-                  PhosphorIcons.userCircle(PhosphorIconsStyle.bold),
+                leading: iconBadge(Icons.style_rounded),
+                title: Text("Flashcards", style: itemStyle()),
+                onTap: () => goToTab(1),
+              ),
+              ListTile(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
                 ),
+                leading: iconBadge(Icons.bar_chart_rounded),
+                title: Text("Progress", style: itemStyle()),
+                onTap: () => goToTab(2),
+              ),
+              ListTile(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                leading: iconBadge(
+                    PhosphorIcons.userCircle(PhosphorIconsStyle.bold)),
                 title: Text("Profile", style: itemStyle()),
-                onTap: () {
-                  Get.back();
-                  Get.until(
-                    (route) {
-                      return Get.currentRoute == AppRoutes.mainRoute;
-                    },
-                  );
-                  mainController.onChangePage(2);
-                },
+                onTap: () => goToTab(3),
               ),
               ListTile(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12),
                 ),
                 leading: iconBadge(
-                  PhosphorIcons.cloudArrowUp(PhosphorIconsStyle.bold),
-                ),
-                title: Text("Create", style: itemStyle()),
+                    PhosphorIcons.cloudArrowUp(PhosphorIconsStyle.bold)),
+                title: Text("Create Deck", style: itemStyle()),
                 onTap: () {
                   Get.back();
-                  Get.until(
-                    (route) {
-                      return Get.currentRoute == AppRoutes.mainRoute;
-                    },
-                  );
-                  mainController.onChangePage(1);
+                  Get.until((route) => Get.currentRoute == AppRoutes.mainRoute);
+                  Get.toNamed(AppRoutes.createDeckRoute);
                 },
               ),
               ListTile(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12),
                 ),
-                leading: iconBadge(
-                  PhosphorIcons.camera(PhosphorIconsStyle.bold),
-                ),
+                leading:
+                    iconBadge(PhosphorIcons.camera(PhosphorIconsStyle.bold)),
                 title: Text("Enter Code", style: itemStyle()),
                 onTap: () {
                   Get.back();
@@ -130,9 +136,8 @@ class AppDrawer extends StatelessWidget {
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12),
                 ),
-                leading: iconBadge(
-                  PhosphorIcons.thumbsUp(PhosphorIconsStyle.bold),
-                ),
+                leading:
+                    iconBadge(PhosphorIcons.thumbsUp(PhosphorIconsStyle.bold)),
                 title: Text("Share This App", style: itemStyle()),
                 onTap: () {
                   Share.share("Checkout this app in https://googleplay.com");
@@ -147,9 +152,8 @@ class AppDrawer extends StatelessWidget {
                     log('Could not launch https://t.me/Ghalia510');
                   }
                 },
-                leading: iconBadge(
-                  PhosphorIcons.question(PhosphorIconsStyle.bold),
-                ),
+                leading:
+                    iconBadge(PhosphorIcons.question(PhosphorIconsStyle.bold)),
                 title: Text("Help & Feedback", style: itemStyle()),
               ),
               ListTile(
@@ -161,9 +165,9 @@ class AppDrawer extends StatelessWidget {
                 },
                 leading: iconBadge(
                   PhosphorIcons.signOut(PhosphorIconsStyle.bold),
-                  badgeColor: Colors.red,
+                  badgeColor: AppColor.errorColor,
                 ),
-                title: Text("Log Out", style: itemStyle(color: Colors.red)),
+                title: Text("Log Out", style: itemStyle(color: AppColor.errorColor)),
               ),
             ],
           ),

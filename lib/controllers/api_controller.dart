29cc19efff4fa3def061e8c8e8b9a4d6d9313d7c
Ezel3815 <skukzleenii@gmail.com>
2026-09-12 21:24:59 +0300

    Update api_controller.dart

--- lib/controllers/api_controller.dart
+++ lib/controllers/api_controller.dart
@@ -11,6 +11,7 @@ import 'package:upgrade/di.dart';
 import 'package:upgrade/entity/card_entity.dart';
 import 'package:upgrade/entity/deck_entity.dart';
 import 'package:upgrade/entity/document_entity.dart';
+import 'package:upgrade/entity/leaderboard_entry.dart';
 import 'package:upgrade/entity/profile_entity.dart';
 import 'package:upgrade/mapper/app_mapper.dart';
 import 'package:upgrade/models/card_model.dart';
@@ -783,6 +784,25 @@ class ApiController {
       }
     }
     return false;
+  }
+
+    static Future<List<LeaderboardEntry>> getLeaderboard() async {
+    try {
+      final response = await dio.get(
+        Api.leaderboard,
+        options: GetOptions.getOptions(),
+      );
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        return (response.data as List)
+            .map((e) => LeaderboardEntry.fromJson(e))
+            .toList();
+      }
+    } catch (e) {
+      if (ErrorHandler.handle(e).failure.code != -6) {
+        showSnackBarWidget(message: ErrorHandler.handle(e).failure.message ?? "");
+      }
+    }
+    return [];
   }
 
     static Future<List<ProfileEntity>> searchUsers(String query) async {

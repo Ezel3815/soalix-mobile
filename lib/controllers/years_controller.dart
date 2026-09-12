0c2cb1623d13a08b6badc7470810128fd4d6f6cd
Ezel3815 <skukzleenii@gmail.com>
2026-09-12 21:38:54 +0300

    Add daily missions functionality to years controller

--- lib/controllers/years_controller.dart
+++ lib/controllers/years_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:get/get.dart';
 import 'package:upgrade/controllers/api_controller.dart';
+import 'package:upgrade/entity/daily_mission.dart';
 import 'package:upgrade/entity/deck_entity.dart';
 import 'package:upgrade/entity/profile_entity.dart';
 import 'package:upgrade/main.dart';
@@ -17,6 +18,7 @@ class YearsController extends GetxController {
   final RxList<DeckEntity> decks = <DeckEntity>[].obs;
 
   final Rx<ProfileEntity?> profile = Rx<ProfileEntity?>(null);
+  final RxList<DailyMission> missions = <DailyMission>[].obs;
 
   bool get loading => _loading.value;
 
@@ -49,11 +51,16 @@ class YearsController extends GetxController {
     profile.value = await ApiController.getProfile(id);
   }
 
+  Future<void> getDailyMissions() async {
+    missions.assignAll(await ApiController.getDailyMissions());
+  }
+
   @override
   void onInit() async {
     await Future.wait([
       getAllDeck(),
       getMyProfile(),
+      getDailyMissions(),
     ]);
     super.onInit();
   }

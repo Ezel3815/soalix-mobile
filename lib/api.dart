8140b0ba43a6d732833e504510575353c7c96b97
Ezel3815 <skukzleenii@gmail.com>
2026-09-12 21:22:55 +0300

    Update api.dart

--- lib/api.dart
+++ lib/api.dart
@@ -1,5 +1,6 @@
 class Api {
   static const String searchUsers = '$baseUrl/users/search';
+  static const String leaderboard = '$baseUrl/users/leaderboard';
   static String viewProfile(int id) => '$baseUrl/users/$id/profile';
   static const String baseUrl = 'https://soalix-backend.onrender.com';
   static const String imageUrl = '$baseUrl/public/';

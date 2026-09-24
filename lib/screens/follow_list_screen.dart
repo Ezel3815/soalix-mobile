f7f466fbe50163d95c0796bb8ce77044936235a9
Ezel3815 <skukzleenii@gmail.com>
2026-09-24 17:29:20 +0300

    Update follow_list_screen.dart

--- lib/screens/follow_list_screen.dart
+++ lib/screens/follow_list_screen.dart
@@ -79,9 +79,7 @@ class _FollowListScreenState extends State<FollowListScreen> {
                     onPressed: Get.back,
                     icon: const Icon(Icons.arrow_back_ios_new_rounded,
                         size: 20,
-                        color: AppColor.textPrimary,
-                        // Real "back" navigation: mirror for RTL.
-                        matchTextDirection: true),
+                        color: AppColor.textPrimary),
                   ),
                   Expanded(
                     child: Column(
